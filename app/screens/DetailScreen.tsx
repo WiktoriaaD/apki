0aa7865f535699c96624865fcc5622a8fe3d8456
Wikaa <dulskaw@gmail.com>
2026-04-10 09:37:12 +0200

    Update DetailScreen.tsx

--- app/screens/DetailScreen.tsx
+++ app/screens/DetailScreen.tsx
@@ -1,38 +1,35 @@
-import ListItem from "@/components/ListItem";
 import { RouteProp } from '@react-navigation/native';
-import { Pressable, ScrollView, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { RootStackParamList } from "../(tabs)/index";
 
-type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
+type DetailsScreenRouteProp = RouteProp<RootStackParamList, "Details">;
 
 type DetailsScreenProps = {
     route: DetailsScreenRouteProp;
 };
 
-export default function DetailScreen({route}: { route: DetailsScreenRouteProp }) {
-    const { title, description } = route.params;
+export default function DetailScreen({route}: DetailsScreenProps) {
+    const { title, description, location,hour} = route.params;
+  //   const events = [
+  //   { id: 1, title: "Wykład React", description: "Sala A1, 10:00", location: "Budynek A" },
+  //   { id: 2, title: "Warsztaty AI", description: "Sala B2, 12:00", location: "Budynek C" },
+  //   { id: 3, title: "Spotkanie Koła", description: "Sala C3, 15:00", location: "Budynek D" },
+  //   { id: 4, title: "Prezentacja Projektów", description: "Sala D4, 17:00", location: "Budynek E" },
+  //   { id: 5, title: "Panel Dyskusyjny", description: "Sala E5, 19:00", location: "Budynek F" },
+  //   { id: 6, title: "Networking", description: "Sala F6, 21:00", location: "Budynek G" },
+  //   { id: 7, title: "Zakończenie", description: "Sala G7, 23:00", location: "Budynek H" },
+  //   { id: 8, title: "After Party", description: "Sala H8, 00:00", location: "Budynek I" },
+  //   { id: 9, title: "Wykład Go", description: "Sala I9, 10:00", location: "Budynek J" },
+  //   { id: 10, title: "Warsztaty DevOps", description: "Sala J10, 12:00", location: "Budynek K" },
+  // ];
 
     return (
-        <ScrollView>
-                {events.map((event) => (
-                  <Pressable
-                    key={event.id}
-                    onPress={() =>
-                      navigation.navigate('Details', {
-                        title: event.title,
-                        description: event.description,
-                      })
-                    }
-                  >
-                    <ListItem
-                      key={event.id}
-                        title={event.title}
-                        description={event.description}
-                        location={event.location}
-                    />
-                  </Pressable>
-                ))}
-        </ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description}>{description}</Text>
+          <Text style={styles.location}>{location}</Text>
+          <Text style={styles.location}>{hour}</Text>
+        </View>
     );
 
 }
@@ -40,13 +37,19 @@ export default function DetailScreen({route}: { route: DetailsScreenRouteProp })
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#e7c4c4ff",
+        backgroundColor: "#fffafaa4",
         alignItems: "center",
         justifyContent: "center",
+        
     },
     title: {
         fontSize: 28,
         fontWeight: "bold",
+    },
+    location
+      : {
+        fontSize: 16,
+        marginTop: 10,
     },
     subtitle: {
         fontSize: 16,
